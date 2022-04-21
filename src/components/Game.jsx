e73a046739f8fcadce59519cc540bbe7caa09fe5
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2022-04-21 11:34:35 -0700

    Movimiento implementado

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,34 +2,18 @@ import React, { useState, useEffect } from "react";
 import reactDom from 'react-dom';
 import InputForLabyrinth from "./Input.jsx";
 
-/* [["+", "-", "-", "+", "-", "-", "+", "-", "-", "+", "-", "-", "+"], 
-    ["|", "p", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"], 
-    ["+", "-", "-", "+", "-", "-", "+", "-", "-", "+", " ", " ", "+"], 
-    ["|", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "|"], 
-    ["+", " ", " ", "+", "-", "-", "+", "-", "-", "+", "-", "-", "+"], 
-    ["|", " ", " ", " ", " ", " ", "|", " ", " ", " ", " ", " ", "|"], 
-    ["+", " ", " ", "+", " ", " ", "+", " ", " ", "+", " ", " ", "+"], 
-    ["|", " ", " ", "|", " ", " ", " ", " ", " ", "|", " ", "g", "|"], 
-    ["+", "-", "-", "+", "-", "-", "+", "-", "-", "+", "-", "-", "+"]] */
+// import '../styles/style.css'
 
 const Game = () => {
                         
     const[labyrinth, setLabyrinth] = useState([])   
     const[ready, setReady] = useState(false)
+    const[finish, setFinish] = useState(false)
     const[weight, setWeight] = useState(4)
     const[height, setHeight] = useState(4)
 
-    let testX = -1
-    let testY = -1
-    const[player, setPlayer] = useState({
-        posX: -1,
-        posY: -1,        
-    })
-
-    const[goal, setGoal] = useState({
-        posX: -2,
-        posY: -2,
-    })
+    let posPlayerX = -1
+    let posPlayerY = -1
     
     const CreateNewLabyrinth = () => {
         setReady(false)
@@ -37,6 +21,7 @@ const Game = () => {
         .then(response => response.json())
         .then(result => {            
             setLabyrinth(result)
+            setFinish(false)
             setReady(true)
         })
         .catch(error => alert("Error inesperado con los shows" + error))
@@ -49,12 +34,8 @@ const Game = () => {
     const InsertElement = ({element, i, j}) => {
 
         if(element === 'p'){
-            console.log("JUGADOR UBICADO EN: " + i + " - " + j)
-            testX = i
-            testY = j
-        }
-        if(element === 'g'){                                                
-            console.log("META UBICADO EN: " + i + " - " + j)            
+            posPlayerX = i
+            posPlayerY = j
         }
         
         return(
@@ -62,44 +43,74 @@ const Game = () => {
         )
     }    
 
-    document.onkeydown = function(e) {
-        if(e.key === "Enter"){                
-            console.log("EMPEZAR | X = " + labyrinth[testX][testY])
-        }    
-    
+    const changuePosition = (defX, defY, x, y) => {
+        if(!finish){
+            const element = labyrinth[x][y]
+            if(element === ' '){
+                console.log("CAMINAR")
+                const renderNewLabyrinth = [...labyrinth]
+                renderNewLabyrinth[defX][defY] = ' '
+                renderNewLabyrinth[x][y] = 'p'
+                posPlayerX = x
+                posPlayerY = y
+                setLabyrinth(renderNewLabyrinth)
+            }
+            else if(element === 'g'){
+                alert("JUEGO TERMINADO")
+                setFinish(true)
+            }
+        }
+    }
+
+    const handleKeyDown = (e) => {        
         if(e.key === "w" || e.key === "W"){
-            console.log("ARRIBA | X = " + labyrinth[testX-1][testY])
+            if(ready){
+                console.log("ARRIBA = " + labyrinth[posPlayerX-1][posPlayerY])
+                changuePosition(posPlayerX, posPlayerY, posPlayerX-1, posPlayerY)
+            }
         }    
         
-        if(e.key === "s" || e.key === "S"){
-            console.log("ABAJO | X = " + labyrinth[testX+1][testY])
+        else if(e.key === "s" || e.key === "S"){
+            if(ready){
+                console.log("ABAJO = " + labyrinth[posPlayerX+1][posPlayerY])
+                changuePosition(posPlayerX, posPlayerY, posPlayerX+1, posPlayerY)
+            }
         }    
         
-        if(e.key === "d" || e.key === "D"){
-            console.log("DERECHA | X = " + labyrinth[testX][testY+1])
+        else if(e.key === "d" || e.key === "D"){
+            if(ready){
+                console.log("DERECHA = " + labyrinth[posPlayerX][posPlayerY+1])
+                changuePosition(posPlayerX, posPlayerY, posPlayerX, posPlayerY+1)
+            }
         }    
         
-        if(e.key === "a" || e.key === "A"){
-            console.log("IZQUIERDA | X = " + labyrinth[testX][testY-1])
+        else if(e.key === "a" || e.key === "A"){
+            if(ready){
+                console.log("IZQUIERDA = " + labyrinth[posPlayerX][posPlayerY-1])
+                changuePosition(posPlayerX, posPlayerY, posPlayerX, posPlayerY-1)
+            }
         }    
-    };
+    }
+    
+    // const title = {
+    //     color: 'red'
+    // }
     
     return (
-        <>
+        <div onKeyDown={handleKeyDown} tabIndex="0">
             {!ready && <h1>Cargando...</h1>}
             {ready && 
                 <div>
-                    <h1>¡QUE EMPIECE EL JUEGO!</h1>            
+                    {/* <h1 style={title}>¡QUE EMPIECE EL JUEGO!</h1>             */}
+                    <h1 className="title">¡QUE EMPIECE EL JUEGO!</h1>            
 
                     <form>
                         <InputForLabyrinth field={"Ancho:"} value={weight} setValue={setWeight} />
                         <InputForLabyrinth field={"Alto:"} value={height} setValue={setHeight} />
                     </form>
-
+                    <br/>
                     <button onClick={CreateNewLabyrinth}>CREAR NUEVO LABERINTO</button>
                     
-                    <h2>POS X: {player.posX} + POS Y: {player.posY}</h2>       
-
                     <table>
                         <tbody>
                             {labyrinth.slice(0, labyrinth.length).map((item, i) => (
@@ -113,8 +124,7 @@ const Game = () => {
                     </table>                    
                 </div>            
             }
-        </>
-        
+        </div>        
     )
 }
 
